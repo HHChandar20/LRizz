4aa948347a6b4898948ec59106b5558d4892624b
HHChandar20 <HHChandar20@codingburgas.bg>
2023-04-04 00:34:50 +0300

    Add constructor to Game

--- game/Game.cpp
+++ game/Game.cpp
@@ -2,6 +2,68 @@
 
 Game::Game()
 {
+	for (int i = 0; i < 3; i++)
+	{
+		reaction[i] = -1;
+	}
+	animationTimer = 0.0f;
+	timePlayed = 0.0f;
+	frame = 0;
+	periodicTable = 0;
+	chemistryShelf = 0;
+	reactor = 0;
+	musicPlayer = 0;
+	onFire = 0;
+	interaction = 0;
+	isMenuOpened = 0;
+	pauseMusic = 1;
+	repeatMusic = 0;
+	packaging = 0;
+	packageClosed = 0;
+	isCarryingBox = 0;
+	isCarryingExtinguisher = 0;
+	mailbox = 0;
+
+	tracks[0] = "lo-fi";
+	tracks[1] = "superhero";
+	tracks[2] = "phonk";
+	tracks[3] = "tourner-dans-le-vide";
+	tracks[4] = "tokyo-drift";
+
+	for (int i = 0; i < 6; i++)
+	{
+		inventoryElementTypes[i] = 0; // 0 - basic element, 1 - product
+		inventoryElements[i] = -1; // Set inventory to empty
+	}
+
+	musicIndex = 0;
+	randomMusic = 0;
+	selectedElement = -1;
+	elementPosition = -1;
+	inventoryIndex = -1;
+	packageElement = -1;
+	orderElement = rand() % 22;
+	inCollision = 0;
+
+	character = { 130, 500 }; //character position (x, y)
+
+	for (int i = 0; i < 7; i++)
+	{
+		fire[i] = RED;
+		if (i < 4)
+			fire[i] = MAROON;
+	}
+	std::string path;
+	for (int i = 0; i < 5; i++)
+	{
+		path = tracks[i] + ".mp3";
+		music[i] = LoadMusicStream(path.c_str());
+	}
+
+	extinguishTime = 0.0f;
+
+	load();
+	PlayMusicStream(music[musicIndex]);
 }
 
 Game::~Game()
